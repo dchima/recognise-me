a5bde6bdcb3a2d098d5fadaf900077315798306e
dchima <dcnnad@gmail.com>
2020-12-14 13:03:38 +0100

    :cyclone: finish

--- src/jobs.js
+++ src/jobs.js
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import '@tensorflow/tfjs-node';
+import * as faceapi from 'face-api.js';
+import * as canvas from 'canvas';
+import { faceDetectionNet, faceDetectionOptions} from './faceDetection';
 import fs from 'fs';
 import path from 'path';
-import * as canvas from 'canvas';
-import * as faceapi from 'face-api.js';
-import { faceDetectionNet, faceDetectionOptions} from './faceDetection';
 
 const { Canvas, Image, ImageData } = canvas;
 faceapi.env.monkeyPatch({ Canvas, Image, ImageData });
@@ -17,7 +17,7 @@ router.get('/face', async (req, res) => {
   try {
     // res.sendFile(path.resolve(__dirname, './index.html'));
   await faceDetectionNet.loadFromDisk(path.resolve(__dirname, '../weights'));
-  const img = await canvas.loadImage('./models/dog.jpg');
+  const img = await canvas.loadImage('./models/face1.jpg');
   const detections = await faceapi.detectSingleFace(img, faceDetectionOptions);
   console.log(typeof detections);
   const value = JSON.parse(JSON.stringify(detections));
@@ -25,10 +25,10 @@ router.get('/face', async (req, res) => {
   
   const out = await faceapi.createCanvasFromMedia(img);
   await faceapi.draw.drawDetections(out, detections);
-  // if (detections[0])
 
   fs.writeFileSync('faceDetection.jpg', out.toBuffer('image/jpeg'));
   console.log('done, saved results to out/faceDetection.jpg');
+  res.send(`image score: ${value._score}`);
   } catch (error) {
     console.log(error);
     console.log('problem with evaluating image, score too low to compute');
